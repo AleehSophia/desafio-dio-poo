16d7c68a324f80babbb66f0d21d93698fc17d463
AleehSophia <alessia.casado@gmail.com>
2023-01-18 18:11:54 -0300

    feat: Bootcamp and Dev

---
+++ src/main/java/org/dio/desafio/domain/Bootcamp.java
@@ -0,0 +1,65 @@
+package org.dio.desafio.domain;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Bootcamp {
+
+    private String name;
+    private String description;
+    private final LocalDate startDate = LocalDate.now();
+    private final LocalDate endDate = startDate.plusDays(45);
+    private Set<Dev> subscribedDevs = new HashSet<>();
+    private Set<Content> contents = new LinkedHashSet<>();
+
+    public Bootcamp() {}
+
+    public Bootcamp(String name, String description, Set<Dev> subscribedDevs, Set<Content> contents) {
+        this.name = name;
+        this.description = description;
+        this.subscribedDevs = subscribedDevs;
+        this.contents = contents;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public Set<Dev> getSubscribedDevs() {
+        return subscribedDevs;
+    }
+
+    public void setSubscribedDevs(Set<Dev> subscribedDevs) {
+        this.subscribedDevs = subscribedDevs;
+    }
+
+    public Set<Content> getContents() {
+        return contents;
+    }
+
+    public void setContents(Set<Content> contents) {
+        this.contents = contents;
+    }
+}
---
+++ src/main/java/org/dio/desafio/domain/Dev.java
@@ -0,0 +1,64 @@
+package org.dio.desafio.domain;
+
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+
+    private String name;
+    private Set<Content> subscribedContents = new LinkedHashSet<>();
+    private Set<Content> finishedContents =  new LinkedHashSet<>();
+
+    public Dev() {}
+
+    public Dev(String name, Set<Content> subscribedContents, Set<Content> finishedContents) {
+        this.name = name;
+        this.subscribedContents = subscribedContents;
+        this.finishedContents = finishedContents;
+    }
+
+    public void subscribeBootcamp(Bootcamp bootcamp) {}
+
+    public void progress() {}
+
+    public void calculateXp() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Content> getSubscribedContents() {
+        return subscribedContents;
+    }
+
+    public void setSubscribedContents(Set<Content> subscribedContents) {
+        this.subscribedContents = subscribedContents;
+    }
+
+    public Set<Content> getFinishedContents() {
+        return finishedContents;
+    }
+
+    public void setFinishedContents(Set<Content> finishedContents) {
+        this.finishedContents = finishedContents;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(name, dev.name) && Objects.equals(subscribedContents, dev.subscribedContents) && Objects.equals(finishedContents, dev.finishedContents);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, subscribedContents, finishedContents);
+    }
+}
